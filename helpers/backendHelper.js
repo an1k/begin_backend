f632ba286c4903a2f7ad72084db8152f51bcfa17
Anik Bose <a-anikb@SINGTELCDC.COM>
2019-12-06 14:17:54 +0800

    added helper

---
+++ helpers/backendHelper.js
@@ -0,0 +1,35 @@
+
+const bayDetails=[
+    {
+        id:1,
+        name:"",
+        organisation:"optus",
+        resource_number:70
+    },
+    {
+        id:2,
+        name:"",
+        organisation:"optus",
+        resource_number:50
+    },
+    {
+        id:3,
+        name:"",
+        organisation:"optus",
+        resource_number:60
+    },
+    {
+        id:4,
+        name:"",
+        organisation:"optus",
+        resource_number:120
+    }
+]
+
+
+
+module.exports.fetchDetailsByBayID=function(id){
+
+    return bayDetails.find(item=>item.id==id)
+
+}
